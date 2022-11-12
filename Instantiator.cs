e991f20ff46c713ab7a45b4d3560eecb04c2ffa1
ixmedrano <52873065+ixmedrano@users.noreply.github.com>
2022-11-11 20:20:51 -0600

    Transaction work
    Started transaction functionality, implemented begin and rollback. Commit is still left to do.

--- Instantiator.cs
+++ Instantiator.cs
@@ -14,16 +14,21 @@ namespace DevotedDatabase
         }
         public Database CreateDatabase()
         {
+            string tempTableName = String.Concat(tableName,"Temporary");
             Database inMemoryDB = new Database();
             inMemoryDB.DatabaseName = databaseName;
             // Instantiate Datatable Object
             inMemoryDB.Table = new List<Table>();
             Table newTable = new Table();
+            Table tempTable = new Table();
             newTable.TableName = tableName;
+            tempTable.TableName = tempTableName;
             // Instantiate Row Object
             newTable.Row = new List<Row>();
+            tempTable.Row = new List<Row>();
             // Add Table Object to Database Object
             inMemoryDB.Table.Add(newTable);
+            inMemoryDB.Table.Add(tempTable);
             return inMemoryDB;
         }
     }
--- Program.cs
+++ Program.cs
@@ -13,21 +13,65 @@ namespace DevotedDatabase
             // Variables
             string databaseName = "DevotedDataBase";
             string tableName = "DevotedDataTable";
+            string tempTableName = String.Concat(tableName,"Temporary");
+            string delimiter = " ";
+            bool transactIndicator = false;
             // Instantiate Database Object
             Database inMemoryDB = new Instantiator(databaseName, tableName).CreateDatabase();
-            // Locate existing table
-            Table table = inMemoryDB.Table.Find(i => i.TableName == tableName);
-            string delimiter = " ";
-
+            // Locate tables
+            Table livetable = inMemoryDB.Table.Find(i => i.TableName == tableName);
+            Table tempTable = inMemoryDB.Table.Find(i => i.TableName == tempTableName);
+            
             var lines = File.ReadLines(incomingFilePath);
             int lineIndex = 1;
             foreach (string line in lines)
-            {
+            {                
                 lineIndex ++;
                 Console.WriteLine(line);
                 string commandType = line.Split(" ")[0];
-                string commandName = line.Split(delimiter)[1];
-                switch (commandType)
+                // Update indicator if transaction has started
+                if (commandType == "BEGIN")
+                {
+                    transactIndicator = true;
+                    continue;
+                }
+                // For roll back command empty object and instantiate a new object
+                if (commandType == "ROLLBACK")
+                {
+                    inMemoryDB.Table.Remove(tempTable);
+                    Table newTempTable = new Table();
+                    newTempTable.TableName = tempTableName;
+                    inMemoryDB.Table.Add(newTempTable);
+                    transactIndicator = false;
+                    continue;
+                }
+                if (commandType == "COMMIT")
+                {
+                    throw new NotImplementedException();
+
+                }
+                // If its not a transaction use live table for updates and gets
+                if (!transactIndicator)
+                {
+                    string commandName = line.Split(delimiter)[1];
+                    Action(commandType, line, delimiter, inMemoryDB, commandName, lineIndex, livetable);
+                    continue;
+                }
+                // If its a transaction use temp table
+                if (transactIndicator)
+                {
+                    string commandName = line.Split(delimiter)[1];
+                    Action(commandType, line, delimiter, inMemoryDB, commandName, lineIndex, tempTable);
+                    continue;
+                }
+                
+
+            }
+        }
+        static void Action(string commandType, string line, string delimiter, Database inMemoryDB, string commandName, int lineIndex
+                           ,Table table)
+        {
+            switch (commandType)
                 {
                     case "SET":
                     string setCommandValue = line.Split(delimiter)[2];
@@ -46,7 +90,7 @@ namespace DevotedDatabase
                     default: Console.WriteLine("Unrecognized Command");
                     break;
                 }
-            }
+
         }
     }
     
